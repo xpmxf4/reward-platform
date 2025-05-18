20e17d1ce2bdc1c08857ce1ef7787c51c6ba5d71
dev-chpark <xpmxf5@gmail.com>
2025-05-18 13:54:35 +0900

    🛠️  config: 인증 서버: main.ts 전역 ValidationPipe 설정

--- auth-server/src/main.ts
+++ auth-server/src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 3000;
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidUnknownValues: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
   await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
