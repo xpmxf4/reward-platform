e2c03ccf1b3cd41f69e2eefc5dfdd3c12afd71bf
dev-chpark <xpmxf5@gmail.com>
2025-05-18 15:54:03 +0900

    ⭐ feat: 인증 서버: AuthModule 정의 및 AppModule에 등록

--- auth-server/src/auth/controller/auth.controller.ts
+++ auth-server/src/auth/controller/auth.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, HttpStatus, UseGuards, Request as NestRequest, Get } from '@nestjs/common';
 import { AuthService } from '../service/auth.service';
+import { CreateUserDto } from '../../users/dto/create-user.dto';
+import { UserDocument } from '../../users/schemas/user.schema';
+import { LocalAuthGuard } from '../guard/local-auth.guard';
+import { LoginUserDto } from '../dto/login-user.dto';
 
-@Controller('auth') // 기본 경로 '/users'
+@Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  @HttpCode(HttpStatus.CREATED)
+  async register(@Body() createUserDto: CreateUserDto): Promise<Omit<UserDocument, 'password'>> {
+    return this.authService.register(createUserDto);
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  async login(@NestRequest() req, @Body() loginUserDto: LoginUserDto ): Promise<{ accessToken: string }> {
+    return this.authService.login(req.user as Pick<UserDocument, 'username' | '_id' | 'roles'>);
+  }
 }
