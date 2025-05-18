8c7cafdf069634eecbff99112dd8195ba022cf19
dev-chpark <xpmxf5@gmail.com>
2025-05-18 13:14:17 +0900

    ⭐ feat: 인증 및 이벤트 서버 초기 MongoDB 연결 설정 및 환경변수 적용

--- auth-server/src/app.module.ts
+++ auth-server/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import {MongooseModule} from "@nestjs/mongoose";
 
 @Module({
-  imports: [],
+  imports: [
+      ConfigModule.forRoot({
+        isGlobal: true,
+        envFilePath: '.env'
+      }),
+      MongooseModule.forRootAsync({
+        imports: [ConfigModule],
+        useFactory: async (configService: ConfigService) => ({
+          uri: configService.get<string>('MONGODB_URI'),
+        }),
+        inject: [ConfigService],
+      })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- event-server/src/main.ts
+++ event-server/src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {ConfigService} from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule);
+    const configService = app.get(ConfigService);
+    const port = configService.get<number>('PORT') || 3000;
+    await app.listen(port);
+    console.log(`Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap();
