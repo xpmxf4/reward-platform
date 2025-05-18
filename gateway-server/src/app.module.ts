5e0817972f4cec77cc613555622537f73e4f4add
dev-chpark <xpmxf5@gmail.com>
2025-05-18 17:02:45 +0900

    🛠️  config: 게이트웨이: ConfigModule 및 HttpModule 설정 확인 및 .env 파일 정의

--- gateway-server/src/app.module.ts
+++ gateway-server/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env', // src 폴더 내 .env 파일
+    }),
+    HttpModule.register({ // HttpModule 기본 설정
+      timeout: 5000,
+      maxRedirects: 5,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+  ],
 })
 export class AppModule {}
