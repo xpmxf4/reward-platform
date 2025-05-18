d83a21874b3a5c74a4ac8b85ff0ca71adb6a4f48
dev-chpark <xpmxf5@gmail.com>
2025-05-18 16:06:17 +0900

    ⭐ feat: 인증 서버: AuthController에 /profile API 엔드포인트 활성화 (JwtAuthGuard 적용)

--- auth-server/src/auth/controller/auth.controller.ts
+++ auth-server/src/auth/controller/auth.controller.ts
@@ -4,6 +4,7 @@ import { CreateUserDto } from '../../users/dto/create-user.dto';
 import { UserDocument } from '../../users/schemas/user.schema';
 import { LocalAuthGuard } from '../guard/local-auth.guard';
 import { LoginUserDto } from '../dto/login-user.dto';
+import { JwtAuthGuard } from '../guard/jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -20,5 +21,13 @@ export class AuthController {
   @HttpCode(HttpStatus.OK)
   async login(@NestRequest() req, @Body() loginUserDto: LoginUserDto ): Promise<{ accessToken: string }> {
     return this.authService.login(req.user as Pick<UserDocument, 'username' | '_id' | 'roles'>);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@NestRequest() req) {
+    // JwtAuthGuard의 validate 메서드가 성공하면 req.user에 사용자 정보가 담김
+    // JwtStrategy의 validate 메서드가 반환한 객체가 여기에 들어옴
+    return req.user;
   }
 }
