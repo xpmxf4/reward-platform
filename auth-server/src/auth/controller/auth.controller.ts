8420b40871f02708b6308f582f648cc1c976df0b
dev-chpark <xpmxf5@gmail.com>
2025-05-18 13:55:45 +0900

    ⭐ feat: 인증 서버: UsersController 기본 골격 추가

--- auth-server/src/auth/controller/auth.controller.ts
+++ auth-server/src/auth/controller/auth.controller.ts
@@ -1,4 +1,7 @@
 import { Controller } from '@nestjs/common';
+import { UsersService } from '../../users/service/users.service';
 
-@Controller('auth')
-export class AuthController {}
+@Controller('users') // 기본 경로 '/users'
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+}
